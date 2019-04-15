9a3f68104af45f0bcff7579f0d998c6efba58ca1
Andrew B <31934606+gochuonpire@users.noreply.github.com>
2019-04-15 11:20:19 -0400

    Update client.js
    Reset radio stations currently being overwritten to prevent commercials/dj breaks. Read cvars for song titles/artists.

--- radio/client.js
+++ radio/client.js
@@ -3,6 +3,30 @@ let isPlaying = false;
 let index = -1;
 let volume = GetProfileSetting(306) / 10;
 let previousVolume = volume;
+let count = 0;
+let rcount = 0;
+let currentTrackId = 0;
+let currentArtist = '';
+
+function updateBadNews(playerRadioStationName) {
+    AddTextEntryByHash(0xFA937B93, GetConvar('current_title' + playerRadioStationName));
+    AddTextEntryByHash(0x4979195D, GetConvar('current_artist' + playerRadioStationName));
+    if(count%10==0) {
+        var int = GetAudibleMusicTrackTextId();
+        console.log("Current track : " + int);
+    }
+}
+
+function resetStations() {
+    var length = customRadios.length;
+    for (let i = 0; i < length; i++) {
+        var customRadio = customRadios[i];
+        FreezeRadioStation(customRadio.name);
+        console.log('resetting : ' + customRadio.name);
+        SetRadioTrack(customRadio.name, "ARM1_RADIO_STARTS");
+        UnfreezeRadioStation(customRadio.name);
+    }
+}
 
 for (let i = 0, length = GetNumResourceMetadata("radio", "supersede_radio"); i < length; i++) {
     const radio = GetResourceMetadata("radio", "supersede_radio", i);
@@ -10,6 +34,8 @@ for (let i = 0, length = GetNumResourceMetadata("radio", "supersede_radio"); i <
     if (!availableRadios.includes(radio)) {
         console.error(`radio: ${radio} is an invalid radio.`);
         continue;
+    } else {
+        updateBadNews(radio);
     }
 
     try {
@@ -60,15 +86,27 @@ const ToggleCustomRadioBehavior = () => {
         StopAudioScene("DLC_MPHEIST_TRANSITION_TO_APT_FADE_IN_RADIO_SCENE");
     }
 };
-
+resetStations();
 setTick(() => {
+    rcount++;
+    if(rcount>=5000) {
+        resetStations();
+        rcount=0;
+    }
     if (IsPlayerVehicleRadioEnabled()) {
+        count++;
         let playerRadioStationName = GetPlayerRadioStationName();
 
         let customRadio = customRadios.find((radio) => {
             return radio.name === playerRadioStationName;
         });
-
+        if(isPlaying && customRadio) {
+            if(count>=200) {
+                //console.log("Updating Song..");
+                updateBadNews(playerRadioStationName);
+                count = 0;
+            }
+        }
         if (!isPlaying && customRadio) {
             PlayCustomRadio(customRadio);
         } else if (isPlaying && customRadio && customRadios.indexOf(customRadio) !== index) {
