9040f4a08de9599fa104ea1d4b14bab62bd19853
Andrew B <31934606+gochuonpire@users.noreply.github.com>
2019-04-15 11:15:29 -0400

    Update __resource.lua

--- radio/__resource.lua
+++ radio/__resource.lua
@@ -1,16 +1,16 @@
-resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+lresource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "https://revolutionradio.ru:8443/live.ogg", volume = 0.2 }
-supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
+supersede_radio "RADIO_01_CLASS_ROCK" { url = "http://azuraURL", volume = 0.1, name = "RADIONAME", api = "azuraapiurl/api/nowplaying/1" }
 
 files {
-	"index.html"
+        "index.html"
 }
 
 ui_page "index.html"
 
 client_scripts {
-	"data.js",
-	"client.js"
+        "data.js",
+        "client.js"
 }
+server_script "server.lua"
